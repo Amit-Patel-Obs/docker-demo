3563e8c3d4cbdda96f92fa61b83c9b5784b18f19
Amit Patel <amitp@observancegroup.com>
2026-04-20 11:09:48 +0530

    docker-update

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get("/", (req, res) => {
   res.send("Hello from Docker CI/CD 🚀");
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
